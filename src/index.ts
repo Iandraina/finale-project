3f9f0a8606a5440863cf25394a4476d21c1f5642
Iandraina <iandraina.ravelomanana@edu.ece.fr>
2019-12-09 14:00:06 +0100

    remove mysql database implementation

--- src/index.ts
+++ src/index.ts
@@ -4,92 +4,55 @@ import bodyparser = require('body-parser')
 import session = require('express-session')
 
 
-
 const app = express()
 /*const authRouter = express.Router()*/
 const port: string = process.env.PORT || '3000'
 
-module.exports = function (app, passport) {
-    //for body parser
-    app.use(express.static(path.join(__dirname, '/../public')))
-    app.use(bodyparser.json())
-    app.use(bodyparser.urlencoded())
+//for body parser
+app.use(express.static(path.join(__dirname, '/../public')))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded())
 
-    //template engine
-    app.set('views', __dirname + "/../views")
-    app.set('view engine', 'ejs');
+//template engine
+app.set('views', __dirname + "/../views")
+app.set('view engine', 'ejs');
 
-    //setting the server up
-    app.listen(port, (err: Error) => {
-        if (err) throw err
-        console.log(`Server is running on http://localhost:${port}`)
-    })
+//setting the server up
+app.listen(port, (err: Error) => {
+    if (err) throw err
+    console.log(`Server is running on http://localhost:${port}`)
+  })
 
-    //home
-    app.get('/', (req: any, res: any) => {
-        res.render('home')
-    })
-    app.get('/home', (req: any, res: any) => {
-        res.render('home')
-    })
+//home
+app.get('/', (req: any, res: any) => {
+    res.render('home')
+})
+app.get('/home', (req: any, res: any) => {
+    res.render('home')
+})
 
-    //login 
-    app.get('/login', (req: any, res: any) => {
-        res.render('login', { message: req.flash('loginMessage') })
-    })
+//login 
+app.get('/login', (req: any, res: any) => {
+    res.render('login')
+})
+// authRouter.get('/login', (req: any, res: any) => {
+//     res.render('login')
+//   })
 
-    app.post('/login', passport.authenticate('local-login',{
-        successRedirect : '/myMetrics',
-        failureRedirect : '/login',
-        failureFlash: true
-    }),
-        function(req, res){
-            if(req.body.remember){
-                req.session.cookie.maxAge = 1000 * 60 * 3
-            }
-            else{
-                req.session.cookie.expires = false
-            }
-            res.redirect('/')
-        })
-    // authRouter.get('/login', (req: any, res: any) => {
-    //     res.render('login')
-    //   })
+//registration
+app.get('/registration', (req: any, res: any) => {
+    res.render('registration')
+})
 
-    //registration
-    app.get('/registration', (req: any, res: any) => {
-        res.render('registration')
-    })
+//signup
+app.get('/signup', (req: any, res: any) => {
+    res.render('registration')
+})
 
-    //signup
-    app.get('/signup', (req: any, res: any) => {
-        res.render('registration', {message: req.flash('signupMessage')})
-    })
-    
-    app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/myMetrics',
-        failureRedirect : '/registration',
-        failureFlash : true
-    }))
+//myMetrics
+app.get('/myMetrics', (req: any, res: any) => {
+    res.render('myMetrics')
+})
 
 
-    //myMetrics
-    app.get('/myMetrics', isLoggedIn, (req: any, res: any) => {
-        res.render('myMetrics', {
-            user:req.user
-        })
-    })
-
-    //logout
-    app.get('logout', function(req,res){
-        req.logout()
-        res.redirect('/home')
-    })
-
-    function isLoggedIn(req, res, next){
-        if(req.isAuthenticated())
-            return next();
-        
-        res.redirect('/home')
-    }
-}
+module.exports = app;
