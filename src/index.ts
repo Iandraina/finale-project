4bbc2e087fd7dd57599fac2ed0ae198200a8c3d2
Iandraina <iandraina.ravelomanana@edu.ece.fr>
2019-12-21 12:09:25 +0100

    database uodate & authentication

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import path = require('path')
 import bodyparser = require('body-parser')
 import session = require('express-session')
 import alert = require('alert-node')
-import { MetricsHandler } from './metrics'
 import Router = require('router')
+import User = require ('../lib/user')
 
 const router = Router()
 const app = express()
@@ -63,89 +63,59 @@ app.get('/login', (req: any, res: any) => {
     res.render('login')
 })
 app.post('/login', (req: any, res: any) => {
-    if (db.collection('details').find({ "userName": req.body.userName })) {
+    var username = req.body.userName
+    var password = req.body.pass
+
+    User.findOne({username: username, password: password}, function(err, user){
+        if(err){
+            console.log(err)
+            return res.status(500).send()
+        }
+
+        if(!user){
+            console.log("diso user")
+            res.render('login')
+            return res.status(404).send()
+        }
+
+        //return res.status(200).send()
         res.render('myMetrics')
-    }
-    else {
-        console.log("the username or password is not correct")
-        res.render('login')
-    }
+    })
 })
+
 //signup page
 app.get('/signup', (req: any, res: any) => {
     res.render('registration')
 })
 app.post('/signup', (req: any, res: any) => {
-     //create schema
-     var Schema = new mongoose.Schema({
-        fName : String,
-        lName : String,
-        userName : {type: String, required: '{PATH} is required.', unique: true},
-        password : String  
-    });
+    var firstName = req.body.fName 
+    var lastname = req.body.lName
+    var username = req.body.userName
+    var password = req.body.pass
 
-    var User = mongoose.model('User', Schema);
-    //add POST values to schema
-    var us = new User({
-        fName: req.body.fName,
-        lName: req.body.lName,
-        userName: req.body.userName,
-        password: req.body.pass
-    //store in database
-    })
-    us.save(function(err, doc) {
-    if (err){
-        res.redirect('/registration')
-        alert('The username you entered is already use')
-        throw err;
-    }
-    else{
-        var ID = db.collection('users').findOne({userName:req.body.userName})._id
-        console.log(ID)
-        req.session.userId = ID 
-        console.log('save user successfully...');
-        res.render('myMetrics')
-    } 
-    });
+    var newUser = new User()
+    newUser.firstName = firstName
+    newUser.lastname = lastname
+    newUser.username = username
+    newUser.password = password
     
-    // if ((db.collection('details').find({ "userName": req.body.userName })) != null) {
-    //     console.log("The username you enter is already used!")
-    //     res.render('registration')
-    // }
-    // else {
-    //     var fname = req.body.fname;
-    //     var lName = req.body.lName;
-    //     var userName = req.body.userName;
-    //     var pass = req.body.pass;
-
-    //     var data = {
-    //         "firstName": fname,
-    //         "lastName": lName,
-    //         "userName": userName,
-    //         "password": pass
-
-    //     }
-    //     db.collection('details').insertOne(data, function (err, collection) {
-    //         if (err) throw err;
-    //         console.log("Record inserted Successfully");
-
-    //     });
-    //     res.render('myMetrics')
-    // }
+    newUser.save(function(err, savedUser){
+        if(err){
+            console.log(err)
+            return res.status(500).send();
+        }
+        else{
+            res.render('myMetrics')
+            return res.status(200).send();
+        }
+    })
 })
 
 //myMetrics page
 app.get('/myMetrics', (req: any, res: any) => {
     res.render('myMetrics')
 })
-app.get('/login', (req: any, res: any) => {
-    MetricsHandler.get((err: Error | null, result?: any) => {
-        if (err) {
-            throw err
-        }
-        res.json(result)
-    })
-})
+
 
 //logout
 router.get('/logout', function(req, res, next) {
