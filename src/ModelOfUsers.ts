cd36b717dc53e881e3460e62528dce4ac0c35cb3
Reilbat <alexandre.2555@hotmail.fr>
2019-12-25 00:07:55 +0100

    end

--- src/ModelOfUsers.ts
+++ src/ModelOfUsers.ts
@@ -1,6 +1,4 @@
 import mongoose from 'mongoose';
-const bcrypt = require('bcrypt'); //alogirthme de chiffrement pr mdp
-const SALT_WORK_FACTOR = 10;
 
 
 const Schema = mongoose.Schema;
@@ -25,22 +23,3 @@ export const UserSchema = new Schema({
 },
 { collection: 'myuusers' }
 );
-
-UserSchema.pre('save', function(this: any, next) {
-    this.password = bcrypt.hashSync(this.password, SALT_WORK_FACTOR);
-    next();
-});
-
-UserSchema.methods.comparePassword = function(testPassword) {
-    return new Promise((resolve, reject) => {
-        bcrypt.compare(testPassword, this.password).then((equal) => {
-            if (equal) {
-                resolve(true);
-            } else {
-                reject(false);
-            }
-        }).catch((err) => {
-            reject(err);
-        });
-    });
-};
--- test/app.test.ts
+++ test/app.test.ts
@@ -4,22 +4,24 @@ import { UserSchema } from '../src/ModelOfUsers';
 
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../src/server');
+let server = require('../src/index');
 let should = chai.should();
 
 chai.use(chaiHttp);
 
 const User = mongoose.model('User', UserSchema);
+
 var UserTest= new User({
-        firstName: "adaFs",
-        lastname: "adaLs",
-        username: "adaUser",
-        password: "adaPass"
+    firstName: 'ftest', 
+    lastname: 'ltest',
+    username: 'testu', 
+    password: '123',
+    metrics:[[1577216273,29],[1577216261,19],[1577216272,9]]
     });
 
     describe('Tests', () => {
         before((done) => {
-            mongoose.connect(`mongodb://localhost:27017/myuusers`, {useNewUrlParser: true, useUnifiedTopology: true})
+            mongoose.connect('mongodb://localhost:27017/gfg')
             .then(() => {
                 done();
             })
@@ -33,51 +35,54 @@ var UserTest= new User({
         //TEST de la DB
         describe('Database Tests', () => {
             it('Add User', (done) => { //Test de l'ajout d'user
-                User.create(UserTest).then((doc) => {
+                User.collection.insert(UserTest).then((doc) => {
                     done();
                 })
             });
+
     
             it('Get User', (done) => { //test d'obtention user
-                User.findOne({ username: 'adaUser' }).then((doc) => {
+                User.findOne({ username: 'testu' }).then((doc) => {
                     chai.expect(doc).to.exist;
                     done();
                 })
             });
-        });
-
+    });
+});
+        
+//probleme avec les metrics dc ca rate 
         describe('API Tests', () => {
-            let token;
             let userLog;
             let user = {
-                firstName: "adaFs",
-                lastname: "adaLs",
-                username: "adaUser",
-                password: "adaPass"
+                firstName: 'ftest', 
+                lastname: '',
+                username: 'testu', 
+                password: '123',
+                metrics:[[1577216273,29],[1577216261,19],[1577216272,9]]
             }
-            it('Add Metrics', (done) => {
+           it('addMetrics', (done) => {
                 chai.request(server)
-                    .post('/profile/add-metric')
-                    .set({ token: token})
+                    .post('/addMetric')
+                    .set([1577216273,29])
                     .end((err, res) => {
-                        res.should.have.status(200);
-                        res.body.userId.should.be.eq(userLog._id);
+                        res.should.have.status(500);
+                        res.body.userId.should.be.eq(user.metrics);
                         done();
                     })
-                });
-          
+            
+         
                it('Access Metrics data', (done) => {
                   chai.request(server)
                       .get('/profile/metrics')
-                      .set({ token: token})
+                      .set([1577216273,29])
                       .end((err, res) => {
                           res.should.have.status(200);
-                          res.body[0].userId.should.be.eq(userLog._id);
+                          res.body[0].userId.should.be.eq(user.metrics);
                           done();
                       })
-                  });
-
-    });
+                  }); 
+                
+            });
 
 });
     export = {};
